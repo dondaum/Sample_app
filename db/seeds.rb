50e7d084b57cea5d0d4d9f702662e68b785df39e
dondaum <sebastian.daum89@gmail.com>
2016-11-19 13:04:30 +0100

    Finish user edit, update, index and destroy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(name:  "Sebastian Daum",
+             email: "sebastian.daum89@gmailcom",
+             password:              "frittenbude",
+             password_confirmation: "frittenbude",
+             admin: true)
